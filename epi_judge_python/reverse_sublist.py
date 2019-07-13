ee0bd1a07a6490378d3b0c35bbc80e77b7e3e556
balooo <discoverx1@gmail.com>
2019-07-13 16:39:01 +0900

    sdfkj

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -7,12 +7,34 @@ def reverse_ll(self, head):
 def reverse_k_sublists(head, k):
     pass
 '''
+class ListNode(object):
+    def __init__(self, val, next):
+        self.val = val
+        self.next = next
 
-def reverse_sublist(L, start, finish):
+def reverse_sublist(head, start, finish):
     # TODO - you fill in here.
+    if start == finish or head is None or head.next is None:
+        return head
+    orig_head = first_half = ListNode(None, head)
+    cur_head = head
     for _ in range(1, start):
-        pass
-    return None
+        first_half = cur_head
+        cur_head = cur_head.next
+
+    # reverse
+    prev, cur = cur_head, cur_head.next
+    for _ in range(finish - start):
+        next = cur.next
+        cur.next = prev
+        prev, cur = cur, next
+
+    # stich
+    new_head = first_half.next
+    first_half.next = prev
+    new_head.next = cur
+
+    return orig_head.next
 
 
 if __name__ == '__main__':
